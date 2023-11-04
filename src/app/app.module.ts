9c86e097baf6cc05c70a3aba6bfc1479c2bf7e0f
Lyden CHAI <lyden.chai@gmail.com>
2023-11-04 11:04:05 +0700

    Design menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { ContainerComponent } from './components/container/container.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./components/app/app.component";
+import { ContainerComponent } from "./components/container/container.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatExpansionModule } from "@angular/material/expansion";
+
 @NgModule({
   declarations: [AppComponent, ContainerComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-container',
-  templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss'],
+  selector: "app-container",
+  templateUrl: "./container.component.html",
+  styleUrls: ["./container.component.scss"],
 })
-export class ContainerComponent {}
+export class ContainerComponent {
+  sideBarOpen = true;
+  desktopViewWidth: number = 1100;
+  drawerMode: "side" | "over" = "side";
+  @HostListener("window:resize", ["$event.target.innerWidth"])
+  onResize(width: number) {
+    if (width < this.desktopViewWidth) {
+      this.drawerMode = "over";
+      if (this.sideBarOpen) {
+        this.sideBarOpen = false;
+      }
+    } else {
+      this.drawerMode = "side";
+      if (!this.sideBarOpen) {
+        this.sideBarOpen = true;
+      }
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log("Hello world!");
+  }
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+
+  drawerOpenChangeHandler = (opened: boolean) => {
+    this.sideBarOpen = opened;
+  };
+
+  onClickMenu() {
+    if (this.drawerMode == "over") {
+      this.sideBarOpen = false;
+    } else {
+      this.sideBarOpen = true;
+    }
+  }
+}
